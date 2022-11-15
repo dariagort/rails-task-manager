4ab1bc7e7032cc735a173e1f4f6ace6fe3b2ee37
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-15 13:58:14 +0100

    create and read tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "tasks#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  get     'tasks',          to: 'tasks#index'
+  get     'tasks/:id/show', to: 'tasks#show', as: :show
+  get     'tasks/new',      to: 'tasks#new', as: :new_task
+  post    'tasks',          to: 'tasks#create'
 end
---
+++ db/migrate/20221115101149_put_complete_as_default_true.rb
@@ -0,0 +1,5 @@
+class PutCompleteAsDefaultTrue < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :tasks, :completed, :boolean
+  end
+end
